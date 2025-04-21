7db6eeba79df5958b4d37a58295216847bf407d9
PRANATHI0077 <poojapranathip@gmail.com>
2025-04-20 23:05:22 -0800

    Add files via upload

---
+++ person details.py
@@ -0,0 +1,21 @@
+def get_person_details():
+    
+    name = input("Enter your name: ")
+    address = input("Enter your address: ")
+    email = input("Enter your email: ")
+    phone = input("Enter your phone number: ")
+    return name, address, email, phone
+
+def print_person_details(name, address, email, phone):
+    
+    print("\n--- Personal Details ---")
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"Phone Number: {phone}")
+
+
+name, address, email, phone = get_person_details()
+
+
+print_person_details(name, address, email, phone)
